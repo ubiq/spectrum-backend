c67e41b14f6bc5460baa6301bc8574f70df948b7
Bitterlox <AngelLoxbit@gmail.com>
2018-10-07 14:39:47 +0200

    Swap out mutex in crawler/charts for channel sync patterns

--- crawler/chart.go
+++ crawler/chart.go
@@ -19,44 +19,59 @@ const DAYS = 14
 
 func (c *Crawler) ChartTxns() {
 	var transaction models.Transaction
-	var mu sync.Mutex
 	var wg sync.WaitGroup
 	var routines int
+
+	start := time.Now()
+	log.Debugf("Start txns gather loop: %v", start)
 
 	iter := c.backend.GetTxnCounts(0)
 
 	data := make(map[string]int)
 
+	// goroutine syncing patter from block crawler
+
+	c1, c2 := make(chan struct{}, 1), make(chan struct{}, 1)
+
 	dates := make([]string, 0)
 	values := make([]string, 0)
+
+	c2 <- struct{}{}
+
+	c1, c2 = c2, make(chan struct{}, 1)
 
 	for iter.Next(&transaction) {
 		wg.Add(1)
 
 		// Transaction is passed by value since each iteration unmarshals a new tx into "transaction"
 
-		go func(mu *sync.Mutex, wg *sync.WaitGroup, tx models.Transaction) {
+		go func(wg *sync.WaitGroup, tx models.Transaction, c1 chan struct{}, c2 chan struct{}) {
+			<-c1
+			close(c1)
+
 			stamp := time.Unix(int64(tx.Timestamp), 0).Format("2/01/06")
 
-			mu.Lock()
 			data[stamp] += 1
-			mu.Unlock()
+
+			c2 <- struct{}{}
 
 			wg.Done()
-		}(&mu, &wg, transaction)
+		}(&wg, transaction, c1, c2)
 
 		routines++
+		c1, c2 = c2, make(chan struct{}, 1)
 
 		if routines == 10 {
 			wg.Wait()
 		}
 	}
 
-	mu.Lock()
+	<-c1
+	close(c1)
+
 	for k, _ := range data {
 		dates = append(dates, k)
 	}
-	mu.Unlock()
 
 	if err := iter.Err(); err != nil {
 		log.Errorf("Error during iteration: %v", err)
@@ -80,18 +95,26 @@ func (c *Crawler) ChartTxns() {
 		}
 
 		c.backend.AddLineChart(doc)
+		log.Debugf("End txns loop: %v", time.Since(start))
+
 	}
 }
 
 func (c *Crawler) ChartBlocks() {
 	var block models.Block
-	var mu sync.Mutex
 	var wg sync.WaitGroup
 	var routines int
+
+	start := time.Now()
+	log.Debugf("Start block gather loop: %v", start)
 
 	iter := c.backend.GetBlocks(0)
 
 	data := make(map[string][]*big.Int)
+
+	// goroutine syncing patter from block crawler
+
+	c1, c2 := make(chan struct{}, 1), make(chan struct{}, 1)
 
 	dates := make([]string, 0)
 
@@ -99,12 +122,19 @@ func (c *Crawler) ChartBlocks() {
 	gaslimit := make([]string, 0)
 	difficulty := make([]string, 0)
 
+	c2 <- struct{}{}
+
+	c1, c2 = c2, make(chan struct{}, 1)
+
 	for iter.Next(&block) {
 		wg.Add(1)
 
 		// Block is passed by value since each iteration unmarshals a new blocks into "block"
 
-		go func(mu *sync.Mutex, wg *sync.WaitGroup, b models.Block) {
+		go func(wg *sync.WaitGroup, b models.Block, c1 chan struct{}, c2 chan struct{}) {
+			<-c1
+			close(c1)
+
 			stamp := time.Unix(int64(b.Timestamp), 0).Format("2/01/06")
 
 			avggasprice := big.NewInt(0)
@@ -115,7 +145,6 @@ func (c *Crawler) ChartBlocks() {
 			gaslimit.SetUint64(b.GasLimit)
 			difficulty.SetString(b.Difficulty, 10)
 
-			mu.Lock()
 			if data[stamp] == nil {
 				data[stamp] = make([]*big.Int, 3)
 				data[stamp][0] = big.NewInt(0)
@@ -125,17 +154,23 @@ func (c *Crawler) ChartBlocks() {
 			data[stamp][0].Add(data[stamp][0], avggasprice)
 			data[stamp][1].Add(data[stamp][1], gaslimit)
 			data[stamp][2].Add(data[stamp][2], difficulty)
-			mu.Unlock()
 
+			c2 <- struct{}{}
 			wg.Done()
-		}(&mu, &wg, block)
+		}(&wg, block, c1, c2)
 
 		routines++
+		c1, c2 = c2, make(chan struct{}, 1)
 
 		if routines == 10 {
 			wg.Wait()
 		}
 	}
+
+	// Wait for loop to end
+
+	<-c1
+	close(c1)
 
 	for k, _ := range data {
 		dates = append(dates, k)
@@ -178,5 +213,7 @@ func (c *Crawler) ChartBlocks() {
 		c.backend.AddLineChart(avggasprice)
 		c.backend.AddLineChart(gaslimit)
 		c.backend.AddLineChart(difficulty)
+
+		log.Debugf("End blocks loop: %v", time.Since(start))
 	}
 }
